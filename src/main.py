4ef70337c1c8ce848f2816522a783e9c03951fe9
csbc-edu <54304912+csbc-edu@users.noreply.github.com>
2023-07-19 16:45:08 +0300

    init commit

---
+++ src/main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def calculate_tariff_payment(tariff_name: int, used_mb: float) -> float:
+    if tariff_name == 5000:
+        return 75 if used_mb <= 5000 else 75 + (used_mb - tariff_name) * 0.02
+    elif tariff_name == 2000:
+        return 35 if used_mb <= 2000 else 35 + (used_mb - tariff_name) * 0.04
+    elif tariff_name == 1000:
+        return 20 if used_mb <= 1000 else 20 + (used_mb - tariff_name) * 0.05
+    else:
+        raise ValueError("Tariff does not exist!")
+
+if __name__ == '__main__':
+    tariff = 1000
+    used_traffic = 800
+    calculate_tariff_payment(tariff, used_traffic)
+
---
+++ test/test_main.py
@@ -0,0 +1,44 @@
+import pytest
+
+from src.main import calculate_tariff_payment
+
+
+def test_calculate_tariff1000_used800():
+    res = calculate_tariff_payment(1000, 800)
+    assert res == 20
+
+def test_calculate_tariff1000_used1200():
+    res = calculate_tariff_payment(1000, 1200)
+    assert res == 30
+
+def test_calculate_tariff1000_used2200():
+    res = calculate_tariff_payment(1000, 2200)
+    assert res == 80
+
+def test_calculate_tariff1000_used5200():
+    res = calculate_tariff_payment(1000, 5200)
+    assert res == 230
+
+def test_calculate_tariff2000_used800():
+    res = calculate_tariff_payment(2000, 800)
+    assert res == 35
+
+def test_calculate_tariff2000_used2200():
+    res = calculate_tariff_payment(2000, 2200)
+    assert res == 43
+
+def test_calculate_tariff2000_used5200():
+    res = calculate_tariff_payment(2000, 5200)
+    assert res == 163
+
+def test_calculate_tariff5000_used800():
+    res = calculate_tariff_payment(5000, 800)
+    assert res == 75
+
+def test_calculate_tariff5000_used5200():
+    res = calculate_tariff_payment(5000, 5200)
+    assert res == 79
+
+def test_calculate_tariff10000_used5200():
+    with pytest.raises(ValueError, match="Tariff does not exist!"):
+        calculate_tariff_payment(10000, 5200)
